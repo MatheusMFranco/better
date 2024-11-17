0e905cc884c27505650de5c9e18a9f11305df5d9
Matheus Franco <14803562+MatheusMFranco@users.noreply.github.com>
2024-11-17 07:07:21 -0300

    feat: add loteria component

---
+++ src/loto/Loto.style.js
@@ -0,0 +1,39 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+    TextDefault: {
+      fontSize: 32,
+      textAlign: 'center',
+      fontWeight: 'bold',
+      padding: 5,
+    },
+    Input: {
+        borderBottomWidth: 1,
+    },
+    Button: {
+        width: '100%',
+        padding: 2,
+        borderWidth: 1,
+        borderColor: '#000',
+    },
+    ButtonText: {
+        textTransform: 'uppercase',
+        color: '#2a5cd1',
+    },
+    NumberContainer: {
+        height: 50,
+        width: 50,
+        margin: 5,
+        borderRadius: 25,
+        backgroundColor: '#000',
+    },
+    NumberText: {
+        color: '#fff',
+    },
+    NumberList: {
+        marginTop: 20,
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        justifyContent: 'center',
+    }
+});
