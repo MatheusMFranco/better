613b0501639ff6986170dc9742efdd54e82ed76e
Matheus Franco <14803562+MatheusMFranco@users.noreply.github.com>
2024-11-21 08:30:30 -0300

    feat: add navigation

---
+++ src/views/Favorites.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, SafeAreaView } from 'react-native';
+
+const Favorites = () => {
+  return (
+    <SafeAreaView>
+      <Text>Favorites Screen</Text>
+    </SafeAreaView>
+  );
+};
+
+export default Favorites;
