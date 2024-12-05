3a5518b8697222fe987e1dfc41e9ac817bbeca9b
Matheus Franco <14803562+MatheusMFranco@users.noreply.github.com>
2024-12-05 05:24:23 -0300

    feat: add checker validation

--- src/views/Checker.tsx
+++ src/views/Checker.tsx
@@ -1,29 +1,59 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import {
-  Text,
   SafeAreaView,
   StyleSheet,
-  ViewStyle,
 } from 'react-native';
+import { Button, Text, Input } from '@rneui/themed';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
-interface Props {}
+const Checker = () => {
+  const [game, setGame] = useState('');
 
-interface State {}
+  const isInvalid = !/^(?!.*\b(\d{2})\b.*\b\1\b)(0[1-9]|[1-5][0-9]|60)([,]?(0[1-9]|[1-5][0-9]|60)){5,11}$/.test(game.replaceAll(' ', ''));
+  const verify = () => game.trim();
 
-export default class Checker extends Component<Props, State> {
+  return (
+    <SafeAreaView style={style.Checker}>
+      <Text h4 style={style.Container}>
+      Enter the winning game:
+      </Text>
+      <Input
+        style={[style.Input, style.Container]}
+        onChangeText={setGame}
+        errorStyle={style.Input}
+        errorMessage={!!game && isInvalid ? `Insert a valid game` : ''}
+      />
+      <Button
+        title=" CHECK YOUR GAME"
+        color="secondary"
+        icon={<Ionicons name='search-circle-outline' size={24} color='white' />}
+        onPress={() => verify()}
+        disabled={isInvalid}
+        containerStyle={style.Action}
+      />
+    </SafeAreaView>
+  );
+};
 
-  render() {
-    return (
-      <SafeAreaView style={styles.container}>
-        <Text>Checker</Text>
-      </SafeAreaView>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: 50,
-  } as ViewStyle,
+const style = StyleSheet.create({
+  Checker: {
+    flexGrow: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    fontSize: 12,
+    textAlign: 'justify',
+  },
+  Container: {
+    margin: 10,
+  },
+  Input: {
+    fontSize: 24,
+    textAlign: 'center',
+    fontWeight: 'bold',
+  },
+  Action: {
+    width: '80%',
+  },
 });
+
+export default Checker;
