cd3253dad50f1ff1ff5060956e14132bc88a3187
Matheus Franco <14803562+MatheusMFranco@users.noreply.github.com>
2024-12-06 06:26:14 -0300

    feat: add storage data

--- src/context/GameContext.tsx
+++ src/context/GameContext.tsx
@@ -1,30 +1,31 @@
-import React, { createContext, useReducer, ReactNode } from 'react';
+import React, { createContext, useReducer, ReactNode, useEffect } from 'react';
 import { GameState } from '../models/GameState.model';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-type ActionType = 'createGame' | 'deleteGame' | 'dailyGame';
+type ActionType = 'createGame' | 'deleteGame' | 'dailyGame' | 'removeAllDailyGame' | 'removeAllSpecialsGame';
 
 interface Action {
   type: ActionType;
-  payload: string;
+  payload: string | string[];
 }
 
 const actions = {
   createGame(state: GameState, action: Action): GameState {
-    const game = action.payload;
+    const game = action.payload as string;
     return {
       ...state,
       specials: [...state.specials, game],
     };
   },
   deleteGame(state: GameState, action: Action): GameState {
-    const gamesToDelete = action.payload;
+    const gamesToDelete = action.payload as string[];
     return {
       ...state,
       specials: state.specials.filter((numbers) => !gamesToDelete.includes(numbers)),
     };
   },
   dailyGame(state: GameState, action: Action): GameState {
-    const game = action.payload;
+    const game = action.payload as string;
     return {
       ...state,
       daily: [...state.daily, { 
@@ -33,15 +34,75 @@ const actions = {
       }],
     };
   },
+  removeAllDailyGame(state: GameState): GameState {
+    return {
+      ...state,
+      daily: [],
+    };
+  },
+  removeAllSpecialsGame(state: GameState): GameState {
+    return {
+      ...state,
+      specials: [],
+    };
+  },
 };
 
-export const GameProvider: React.FC<{ children: ReactNode }> = (props) => {
+export const GameProvider: React.FC<{ children: ReactNode }> = props => {
+  const [state, dispatch] = useReducer(reducer, { specials: [], daily: [] });
+
+  useEffect(() => {
+    const loadState = async () => {
+      try {
+        const specials = await AsyncStorage.getItem('specials');
+        const daily = await AsyncStorage.getItem('daily');
+        if (specials) {
+          const specialsData = JSON.parse(specials);
+          specialsData.forEach((game: string) => {
+            dispatch({
+              type: 'createGame',
+              payload: game,
+            });
+          });
+        }
+        if (daily) {
+          const dailyData = JSON.parse(daily);
+          dailyData.forEach((game: { numbers: string }) => {
+            dispatch({
+              type: 'dailyGame',
+              payload: game.numbers,
+            });
+          });
+        }
+      } catch (error) {
+        console.error('Erro ao carregar dados', error);
+      }
+    };
+
+    loadState();
+  }, []);
+
+  const saveStateToStorage = async (newState: GameState) => {
+    try {
+      await AsyncStorage.setItem('specials', JSON.stringify(newState.specials));
+      await AsyncStorage.setItem('daily', JSON.stringify(newState.daily));
+    } catch (error) {
+      console.error('Erro ao salvar no AsyncStorage', error);
+    }
+  };
+
   function reducer(state: GameState, action: Action): GameState {
+    let newState = state;
     const fn = actions[action.type];
-    return fn ? fn(state, action) : state;
+    if (fn) {
+      newState = fn(state, action);
+    }
+    return newState;
   }
 
-  const [state, dispatch] = useReducer(reducer, { specials: [],  daily: []});
+  useEffect(() => {
+    saveStateToStorage(state);
+  }, [state]);
 
   return (
     <GameContext.Provider value={{ state, dispatch }}>
@@ -54,7 +115,7 @@ const GameContext = createContext<{
   state: GameState;
   dispatch: React.Dispatch<Action>;
 }>({
-  state: { specials: [], daily: []},
+  state: { specials: [], daily: [] },
   dispatch: () => {},
 });
 
